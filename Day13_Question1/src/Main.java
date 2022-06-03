16ccb742d3587744c2d49dcb66b9b7b4b7ead267
ProtyushB <protyushprincebanerjee@gmail.com>
2022-06-03 21:15:52 +0530

    Day13

---
+++ Day13_Question1/src/Main.java
@@ -0,0 +1,34 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String args[]) {
+        try {
+            System.out.println("start of main..");
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Enter num1");
+            int num1 = sc.nextInt();
+            System.out.println("Enter num2");
+            int num2 = sc.nextInt();
+            String message = null;
+            int num3 = num1 / num2;
+            if(num3 > 10){
+                message = "Welcome to Exception Handling ";
+            }
+            System.out.println("Message is :"+message.toUpperCase());
+            System.out.println("end of main");
+        }
+        catch (InputMismatchException inputMismatchException){
+            System.out.println("Please enter valid number");
+        }
+        catch (ArithmeticException arithmeticException){
+            System.out.println("Num2 should not be 0");
+        }
+        catch ( NullPointerException nullPointerException){
+            System.out.println("String value is null");
+        }
+        catch (Exception e){
+            System.out.println("WELCOME TO EXCEPTION HANDLING");
+        }
+    }
+}
